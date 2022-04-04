65a9b2deeb2f1e0514e29d6488b470f0b02d4e90
antdev1987 <antdev1987@gmail.com>
2022-04-04 02:22:25 -0600

    final changes to the load in login page done

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,14 +1,16 @@
 import React, { useState } from "react";
 import { useUser } from "../context/userContext/UserProvider";
 
-import { Link,useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 import { Formik, Form, Field } from "formik";
 import * as Yup from "yup";
-import Swal from 'sweetalert2'
+import Swal from "sweetalert2";
 import axios from "axios";
 import Alerta from "../Components/Alerta";
 
+import LoadingOverlay from "react-loading-overlay";
+LoadingOverlay.propTypes = undefined;
 
 //schema of validation for formik and yup
 const LoginSchema = Yup.object().shape({
@@ -17,85 +19,105 @@ const LoginSchema = Yup.object().shape({
 });
 
 const LoginPage = () => {
+  const { setUser } = useUser();
+  const [alerta, setAlerta] = useState("");
+  const [isActive, setIsActive] = useState(false);
 
-  const {setUser} = useUser()
-  const [alerta,setAlerta] = useState('')
- 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
-  const login = async(values)=>{
-
+  //this happen when user click the button login
+  const login = async (values) => {
+    console.log("al inicio");
+    setIsActive(true);
     try {
-      const url = `${process.env.REACT_APP_API_URL}/api/users/login`
+      const url = `${process.env.REACT_APP_API_URL}/api/users/login`;
 
-      const {data} = await axios.post(url,values)
-      console.log(data)
-      localStorage.setItem('uid',JSON.stringify(data.data))
+      const { data } = await axios.post(url, values);
+      localStorage.setItem("uid", JSON.stringify(data.data));
 
-      setUser(data.data)
+      if (data.data.name) {
+        setIsActive(false);
+      }
+
+      setUser(data.data);
 
       Swal.fire({
-        icon: 'success',
+        icon: "success",
         title: data.msg,
         showConfirmButton: false,
-        timer: 1500
-    })
+        timer: 1500,
+      });
 
-    navigate('/snippet')
-
+      navigate("/snippet");
     } catch (error) {
-      console.log(error)
-      setAlerta(error.response.data.msg)
+      setIsActive(false);
+      console.log(error);
+      setAlerta(error.response.data.msg);
     }
-
-  }
-
-  console.log('in login page')
+  };
 
   return (
-    <div className="form-box border m-auto mt-5 shadow p-4">
-      <h1 className="mb-3">LogIn</h1>
     
-      {alerta && <Alerta>{alerta}</Alerta>}
-
-      <Formik
-        initialValues={{
-          email: "",
-          password: "",
-        }}
-        validationSchema={LoginSchema}
-        onSubmit={(values) => {
-          // same shape as initial values
-          login(values)
-        }}
+      <LoadingOverlay
+        className="w-100 p-5 min-vh-94"
+        active={isActive}
+        spinner
+        text="Loading your content..."
       >
-        {({ errors, touched }) => (
-          <Form>
-            <div className="mb-3">
-              <label className="form-label">Email</label>
-              <Field name="email" className="form-control" />
-              {errors.email && touched.email ? <div className="text-center mt-2 bg-warning fw-bold p-1 rounded">{errors.email}</div> : null}
-            </div>
+        <div className="form-box border m-auto  shadow p-4">
+          <h1 className="mb-3">LogIn</h1>
 
-            <div className="mb-3">
-              <label className="form-label">Password</label>
-              <Field name="password" className="form-control" type="password" />
-              {errors.password && touched.password ? (
-                <div className="text-center mt-2 bg-warning fw-bold p-1 rounded">{errors.password}</div>
-              ) : null}
-            </div>
+          {alerta && <Alerta>{alerta}</Alerta>}
 
-            <button type="submit" className="btn btn-primary">
-              Submit
-            </button>
-          </Form>
-        )}
-      </Formik>
+          <Formik
+            initialValues={{
+              email: "",
+              password: "",
+            }}
+            validationSchema={LoginSchema}
+            onSubmit={(values) => {
+              login(values);
+            }}
+          >
+            {({ errors, touched }) => (
+              <Form>
+                <div className="mb-3">
+                  <label className="form-label">Email</label>
+                  <Field name="email" className="form-control" />
+                  {errors.email && touched.email ? (
+                    <div className="text-center mt-2 bg-warning fw-bold p-1 rounded">
+                      {errors.email}
+                    </div>
+                  ) : null}
+                </div>
 
-      <Link className="mt-4 d-block text-decoration-none" to="/register">
-        Register
-      </Link>
-    </div>
+                <div className="mb-3">
+                  <label className="form-label">Password</label>
+                  <Field
+                    name="password"
+                    className="form-control"
+                    type="password"
+                  />
+                  {errors.password && touched.password ? (
+                    <div className="text-center mt-2 bg-warning fw-bold p-1 rounded">
+                      {errors.password}
+                    </div>
+                  ) : null}
+                </div>
+
+                <button type="submit" className="btn btn-primary">
+                  Submit
+                </button>
+              </Form>
+            )}
+          </Formik>
+
+          <Link className="mt-4 d-block text-decoration-none" to="/register">
+            Register
+          </Link>
+        </div>
+      </LoadingOverlay>
+    
   );
 };
 
